f3aac345d46c3213aa28b63f9692f67eb038ed98
Chanchal <chanchalmandal146@gmail.com>
2023-06-28 18:24:10 +0530

    Added more v

--- Uart_transmitter_tb.v
+++ Uart_transmitter_tb.v
@@ -62,7 +62,7 @@ module UART_TB ();
   initial 
   begin
     // Required to dump signals to EPWave
-    $dumpfile("trans.vcd");
+    $dumpfile("Uart_transmitter.vcd");
     $dumpvars(0,UART_TB);
   end
 endmodule
